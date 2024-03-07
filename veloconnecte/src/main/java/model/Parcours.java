945f6855777a0492baf46a703cc4358cf254db31
cceren <cceren@norsys.fr>
2024-03-07 12:08:56 +0100

    Init des models

---
+++ veloconnecte/src/main/java/model/Parcours.java
@@ -0,0 +1,35 @@
+package model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Entity
+@Table(name = "parcours")
+@Getter
+@Setter
+public class Parcours {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @Column(name = "destination")
+    private String destination;
+
+    @Column(name = "distance")
+    private float distance;
+
+    @OneToOne(orphanRemoval = true)
+    @JoinColumn(name = "owner_id")
+    private Utilisateur owner;
+
+    @OneToMany(orphanRemoval = true)
+    @JoinColumn(name = "parcours_id")
+    private List<Utilisateur> participants = new ArrayList<>();
+
+}
