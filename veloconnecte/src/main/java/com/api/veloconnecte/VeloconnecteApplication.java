df8f848e86457a9ad0bc4135a39c475ee43e188f
cceren <cceren@norsys.fr>
2024-03-07 10:42:15 +0100

    init du projet veloconnecte

---
+++ veloconnecte/src/main/java/com/api/veloconnecte/VeloconnecteApplication.java
@@ -0,0 +1,13 @@
+package com.api.veloconnecte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeloconnecteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VeloconnecteApplication.class, args);
+    }
+
+}
